5a2bd51fc6bae0df294ff8ea50d242997b1459dc
Arthur Gibbs <arthurgibbs@gmail.com>
2016-03-26 09:35:47 +0000

    added code

---
+++ WaterPot/WaterPot.ino
@@ -0,0 +1,87 @@
+
+int red=4;
+int green = 2
+int blue = 3;
+int moisture = 0;
+int detectPowerPin = 5;
+int pumpPin = 6;
+int analogPin = 0;
+int low= 1
+int med= 2
+int high = 3
+
+
+int interval = 1000;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(detectPowerPin, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(pumpPin, OUTPUT);  
+  
+  
+
+  
+  Serial.begin(9600);
+}
+
+void blink(int ont) {
+ 
+  digitalWrite(2, 1);//green
+  digitalWrite(3, 1);//blue
+  digitalWrite(4, 0);//red
+  
+  delay(ont);  
+  digitalWrite(2, 1);
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(detectPowerPin, 1);
+  delay(100); 
+  int invMoisture = analogRead(analogPin);
+  int seclev = analogRead(1);
+  Serial.println(seclev); 
+  
+  digitalWrite(detectPowerPin, 0);
+  moisture = 1023 - invMoisture;
+  
+  //423 = air
+  //650 = finger grip
+  //900 = 1 cm water
+  //950 = full submersion 
+  if (moisture < 600) { 
+    blink(50);
+    delay(100);
+    blink(50);
+  }
+  
+  if (moisture < 500) { 
+    digitalWrite(pumpPin, 1);
+   // analogWrite(pumpPin, 1);
+   // interval = 1000;
+  }
+  else {
+    digitalWrite(pumpPin, 0);
+   // interval = 5000;
+  }
+  
+  delay(interval);
+}
+
+int waterlevel(){
+    //set pullup resistors
+  digitalWrite(2, 1);
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  int l1 = analogRead(low);
+  
+}
+
